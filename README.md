35d6b6dad3e449e59156881bf67a9b6ee0bc58f2
MaxwellFortney <MaxwellFortney@gmail.com>
2019-09-02 20:12:01 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Maxwell Fortney's arch i3 dotfiles.
---
+++ scripts/workspaces-init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sleep.5
+i3-msg 'workspace 3; exec urxvt, exec pcmanfm'
+sleep .5
+i3-msg 'workspace 2; exec urxvt -e sh -c "sudo code"'
+sleep .5
+i3-msg 'workspace 1; exec firefox'
+sleep .5
