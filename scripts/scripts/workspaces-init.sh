ec23bad8f4d3df177e1daf163bdf1319a6d6722e
maxwellfortney <MaxwellFortney@gmail.com>
2019-09-04 20:30:14 +0000

    Fixed more stuff

--- scripts/scripts/workspaces-init.sh
+++ scripts/scripts/workspaces-init.sh
@@ -5,5 +5,5 @@ i3-msg 'workspace 3; exec urxvt, exec pcmanfm'
 sleep .5
 i3-msg 'workspace 2; exec urxvt -e sh -c "sudo code"'
 sleep .5
-i3-msg 'workspace 1; exec firefox'
+i3-msg 'workspace 1; exec google-chrome-stable'
 sleep .5
