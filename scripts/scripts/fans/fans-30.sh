761e17c611fa744a6806ddcb1ab771ee42560c09
maxwellfortney <MaxwellFortney@gmail.com>
2019-09-09 19:26:33 -0400

    Fixed more stuff

--- scripts/scripts/fans/fans-30.sh
+++ scripts/scripts/fans/fans-30.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#/usr/bin/env sh
 sudo liquidctl set fan1 speed 30
 sudo liquidctl set fan2 speed 30
 sudo liquidctl set fan3 speed 30
--- scripts/scripts/xrandr.sh
+++ scripts/scripts/xrandr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sleep 2
-xrandr --output DP-1 --mode 2560x1440 --rate 60.00
+xrandr --output DisplayPort-0 --mode 2560x1440 --rate 60.00
 sleep 2
-xrandr --output DP-1 --mode 2560x1440 --rate 144.00
+xrandr --output DisplayPort-0 --mode 2560x1440 --rate 144.00
