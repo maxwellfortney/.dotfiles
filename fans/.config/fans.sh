5b16b2480a3152fc78febbfacc11a8546745720c
maxwellfortney <MaxwellFortney@gmail.com>
2019-09-13 16:42:38 -0400

    Updates

--- fans/.config/fans.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-sudo liquidctl set fan1 speed 30
-sudo liquidctl set fan2 speed 30
-sudo liquidctl set fan3 speed 30
--- scripts/scripts/xrandr.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sleep 2
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 60.00
-sleep 2
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 144.00
