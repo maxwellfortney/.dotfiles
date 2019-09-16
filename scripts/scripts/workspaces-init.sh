05e388f35af2d1770b2c568d1957a591b9116538
maxwellfortney <MaxwellFortney@gmail.com>
2019-09-15 20:59:50 -0400

    Stuff

--- scripts/scripts/workspaces-init.sh
+++ scripts/scripts/workspaces-init.sh
@@ -1,10 +1,10 @@
 #/usr/bin/env bash
 
-sleep.5
-i3-msg 'workspace 3; exec urxvt, exec pcmanfm'
-i3-msg 'workspace 2; exec urxvt -e sh -c "echo '1208' | sudo -S code"'
+
+i3-msg 'workspace 3; exec --no-startup-id urxvt, exec --no-startup-id pcmanfm'
+sleep 1
+i3-msg 'workspace 2; exec --no-startup-id urxvt -e sh -c "echo '1208' | sudo -S code"'
+sleep 2
+i3-msg 'workspace 4; exec --no-startup-id spotify'
 sleep 3
-i3-msg 'workspace 1; exec google-chrome-stable'
-sleep .5
-wmctrl -r "pcmanfm" -b remove,demands_attention
-wmctrl -r "urxvt" -b remove,demands_attention
+i3-msg 'workspace 1; exec --no-startup-id brave'
