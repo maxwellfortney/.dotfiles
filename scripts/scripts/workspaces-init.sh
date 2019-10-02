c2c387417aba3ac9ad881df79d497314dfd02803
maxwellfortney <MaxwellFortney@gmail.com>
2019-10-02 13:19:04 -0400

    Fixed polybar monitor changing

--- scripts/scripts/workspaces-init.sh
+++ scripts/scripts/workspaces-init.sh
@@ -3,7 +3,8 @@
 
 i3-msg 'workspace 3; exec --no-startup-id urxvt, exec --no-startup-id pcmanfm'
 sleep 1
-i3-msg 'workspace 2; exec --no-startup-id urxvt -e sh -c "echo '1208' | sudo -S code"'
+#i3-msg 'workspace 2; exec --no-startup-id urxvt -e sh -c "echo '1208' | sudo -S code"'
+i3-msg 'workspace 2; exec --no-startup-id urxvt -hold -e vim'
 sleep 2
 i3-msg 'workspace 4; exec --no-startup-id spotify'
 sleep 3
